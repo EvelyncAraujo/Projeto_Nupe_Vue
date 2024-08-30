5aed9b5fc0b3747fdf7ef41e14ffc9cc68c414e5
JuliaOliveira26 <julinhalves2006@gmail.com>
2024-08-30 16:23:00 -0300

    correção erro bulma

--- src/main.js
+++ src/main.js
@@ -2,21 +2,22 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import store from './store/index.js'
 
 //import 'buefy/dist/buefy.css'
-// import './assets/scss/main.scss  
+import './assets/scss/main.scss'
 
-//import Vuex from 'vuex'
+// import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
 import Buefy from 'buefy'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(Buefy)
+app.use(store)
 app.use(router)
-//app.use(Vuex)
+app.use(Buefy)
 
-import "./store/index.js"
+// import "./store/index.js"
 
 app.mount('#app')
