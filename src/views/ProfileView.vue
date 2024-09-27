7041896623f51cf0827597bccea5137e7f4c69ff
evelyncaraujo <evyaraujoifc@gmail.com>
2024-09-27 16:05:44 -0300

    tela-perfil

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -11,32 +11,30 @@ export default {
         senha: '',
         confirmarSenha: ''
       }
-    };
+    }
   },
   methods: {
     checkPasswords() {
       if (this.form.senha === this.form.confirmarSenha) {
-        alert('Senhas correspondem!');
+        alert('Senhas correspondem!')
       } else {
-        alert('As senhas não correspondem. Tente novamente.');
+        alert('As senhas não correspondem. Tente novamente.')
       }
     }
   }
-};
-
+}
 </script>
-  
+
 <template>
   <div class="perfil-Info">
-    <h1>Atualizar dados institucionais</h1>
+    <h1 class="atz">Atualizar dados institucionais</h1>
     <form @submit.prevent="submitForm"></form>
     <div class="forms">
       <FormsProfileView />
     </div>
 
-
     <div class="perfil-aluno">
-      <h1>Foto de perfil</h1>
+      <h1 class="atz">Foto de perfil</h1>
       <img src="/public/circuloImagem.jpeg" alt="Foto do Aluno" class="perfil-foto" />
       <h2>{{ aluno.nome }}</h2>
       <p><strong>Arquivo:</strong> {{ aluno.uploadfoto }}</p>
@@ -47,24 +45,39 @@ export default {
       <form @submit.prevent="submitForm">
         <div class="form-group">
           <label for="email">Email:</label>
-          <input type="email" id="email" v-model="form.email" placeholder="Digite seu email" required />
+          <input
+            type="email"
+            id="email"
+            v-model="form.email"
+            placeholder="Digite seu email"
+            required
+          />
         </div>
         <div class="form-group">
           <label for="senha">Senha:</label>
-          <input type="password" id="senha" v-model="form.senha" placeholder="Digite sua senha" required />
+          <input
+            type="password"
+            id="senha"
+            v-model="form.senha"
+            placeholder="Digite sua senha"
+            required
+          />
         </div>
         <div class="form-group">
           <label for="confirmar-senha">Confirmar Senha:</label>
-          <input type="password" id="confirmar-senha" v-model="form.confirmarSenha" placeholder="Confirme sua senha"
-            required />
+          <input
+            type="password"
+            id="confirmar-senha"
+            v-model="form.confirmarSenha"
+            placeholder="Confirme sua senha"
+            required
+          />
         </div>
-        <button type="submit">Enviar</button>
+        <button type="submit">Confirmar</button>
         <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
       </form>
     </div>
   </div>
-
-
 </template>
 
 <style scoped>
@@ -74,17 +87,16 @@ export default {
   margin-left: 35rem;
   padding: 20px;
   text-align: center;
-  border: 2px solid #000000;
+  border: none;
 }
 
 .perfil-Info {
-  background-color: rgb(158, 154, 149);
   max-width: 600px;
   margin-top: 15rem;
   margin-left: 35rem;
   padding: 20px;
   text-align: center;
-  border: 2px solid #000000;
+  border: none;
 }
 
 .perfil-foto {
@@ -106,12 +118,15 @@ h1 {
   max-width: 600px;
   margin: 2rem auto;
   padding: 20px;
-  border: 2px solid #000000;
+
   text-align: center;
+}
+.perfil-Info {
+  border-radius: 10px;
 }
 
 .perfil-Info {
-  background-color: rgb(151, 136, 117);
+  background-color: rgba(215, 235, 213, 0.322);
 }
 
 .perfil-foto {
@@ -126,17 +141,6 @@ h1 {
   text-align: left;
   font-size: larger;
 }
-
-/* 
-.formulario-cadastro {
-  max-width: 400px;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  background-color: #fff;
-} */
 
 .form-group {
   margin-bottom: 15px;
@@ -159,18 +163,23 @@ button {
   padding: 10px;
   border: none;
   border-radius: 4px;
-  background-color: #3528a7;
+  background-color: #218838;
   color: #fff;
   font-size: 16px;
   cursor: pointer;
 }
 
 button:hover {
-  background-color: #218838;
+  background-color: #176327;
 }
 
 .error {
   color: #dc3545;
   font-size: 14px;
+}
+.atz {
+  text-align: center;
+  color: black;
+  font-size: larger;
 }
 </style>
