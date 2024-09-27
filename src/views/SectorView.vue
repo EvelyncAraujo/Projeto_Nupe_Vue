e547f22ea48e839cb91eccd75c20d7f8c81bf42b
JuliaOliveira26 <julinhalves2006@gmail.com>
2024-09-27 16:09:36 -0300

    pagina_sector

---
+++ src/views/SectorView.vue
@@ -0,0 +1,62 @@
+<script >
+import { ref } from "vue";
+import ListSector from "@/components/register/Sector/ListSector";
+import CreateSector from "@/components/register/Sector/CreateSector";
+import SelectSector from "@/components/register/Sector/SelectSector";
+
+export default {
+  components: { ListSector, CreateSector, SelectSector },
+  setup() {
+    const editing = ref(false);
+    const currentSector = ref({});
+    const creating = ref(false);
+    const value = ref("");
+
+    const createSector = (val) => {
+      creating.value = val;
+    };
+
+    const editSector = (sector) => {
+      editing.value = true;
+      currentSector.value = sector;
+    };
+
+    const cancelEdit = () => {
+      editing.value = false;
+      currentSector.value = {};
+    };
+
+    return {
+      editing,
+      currentSector,
+      creating,
+      value,
+      createSector,
+      editSector,
+      cancelEdit,
+    };
+  },
+};
+</script>
+
+<template>
+    <section class="section is-main-section">
+      <div class="is-vcentered">
+        <select-sector
+          v-if="editing"
+          :sector="currentSector"
+          @cancelEdit="cancelEdit"
+        />
+        <create-sector
+          v-else-if="creating"
+          @createSector="createSector"
+        ></create-sector>
+        <list-sector
+          v-else
+          @createSector="createSector"
+          :value="value"
+          @editSector="editSector"
+        />
+      </div>
+    </section>
+  </template>
