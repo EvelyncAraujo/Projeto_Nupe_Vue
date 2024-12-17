d10ed20fd7a86627ef21cf24cc163894b56fd553
rafaaulbrich <rafaelacamilly2018@gmail.com>
2024-12-17 08:48:19 -0300

    Feito a página de turmas

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -15,9 +15,50 @@ export default {
 </script>
 
 <template>
-    <div class="lista">
-        <p v-for="stu of studentStore.students" :key="stu.id">{{ stu.name }}</p>
-    </div>    
+  <div class="lista">
+    <div v-for="stu of studentStore.students" :key="stu.id" class="card">
+      <div>
+        <div class="img"></div>
+      </div>
+
+      <div class="info">
+        <p>{{ stu.full_name }}</p>
+        <p>Matricula: {{ stu.registration }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.lista {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 20px;
+  justify-content: center;
+}
+
+.card {
+  display: flex;
+  padding: 20px 10px;
+  border-radius: 15px;
+  background-color: #dddddd;
+  width: 300px;
+  gap: 10px;
+}
+
+.img {
+  width: 80px;
+  height: 80px;
+  background-color: white;
+  border-radius: 50%;
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+}
+</style>
